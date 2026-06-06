2820e7c1d954e6be272a6d7b3f3145e351beba48
v1tooR <victorssantos572@gmail.com>
2026-06-06 01:12:08 -0300

    ajuste auth2

---
+++ server.js
@@ -0,0 +1,20 @@
+const { createServer } = require("node:http");
+const next = require("next");
+
+const port = Number.parseInt(process.env.PORT || "3000", 10);
+const hostname = process.env.HOSTNAME || "0.0.0.0";
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, () => {
+    console.log(
+      `> Server listening at http://${hostname}:${port} as ${
+        dev ? "development" : process.env.NODE_ENV
+      }`
+    );
+  });
+});
