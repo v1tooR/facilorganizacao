7208fb09b13aebdfefe660bef56020c5389cf57a
v1tooR <victorssantos572@gmail.com>
2026-06-06 01:26:06 -0300

    auth3

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const { createServer } = require("http");
 const next = require("next");
 
-const port = Number.parseInt(process.env.PORT || "3000", 10);
+const port = Number.parseInt(
+  process.env.PORT ||
+    process.env.PASSENGER_PORT ||
+    process.env.NODE_PORT ||
+    process.env.APP_PORT ||
+    "3000",
+  10
+);
 const hostname = process.env.HOST || "0.0.0.0";
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
+
+console.log(
+  `> Booting with PORT=${process.env.PORT || ""} PASSENGER_PORT=${
+    process.env.PASSENGER_PORT || ""
+  } NODE_PORT=${process.env.NODE_PORT || ""} APP_PORT=${
+    process.env.APP_PORT || ""
+  }`
+);
 
 app.prepare().then(() => {
   const server = createServer((req, res) => {
